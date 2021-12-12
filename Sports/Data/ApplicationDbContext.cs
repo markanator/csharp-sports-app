67e57c7acf25b5f95b5f219f3038b2f32e53315d
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2021-12-12 15:28:40 -0700

    adding psql

--- Sports/Data/ApplicationDbContext.cs
+++ Sports/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace SportsMVC.Data
 {
     // create db connection
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
--- Sports/Startup.cs
+++ Sports/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SportsLib.Interfaces;
+using SportsMVC.Data;
 using SportsMVC.Models;
 using System;
 using System.Collections.Generic;
@@ -25,6 +27,9 @@ namespace Sports
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
 
             // singletons for static data
