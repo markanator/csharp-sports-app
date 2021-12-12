0d9c0a302484ceb95172ceda080cc82fcb9f78b2
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2021-12-12 00:41:00 -0700

    rename for clarity

--- Sports/Startup.cs
+++ Sports/Startup.cs
@@ -24,6 +24,7 @@ namespace Sports
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
